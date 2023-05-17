f984631ebe13deb6b7423d01e2d413bbfb5ca8b5
dylanquaale <dylanquaale33@gmail.com>
2023-05-16 22:01:40 -0500

    tailwind working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
-
-
 export default function Home() {
   return (
-    <div>
+    <div className='text-3xl text-sky-500'>
       Hello World!
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsk,tsk}',
-    './pages/**/*.{js,ts,jsk,tsk}',
-    './components/**/*.{js,ts,jsk,tsk}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
