47363798568ab301340ceb1ab289089444ccc620
dylanquaale <dylanquaale33@gmail.com>
2023-05-16 21:26:21 -0500

    tailwind set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './app/**/*.{js,ts,jsk,tsk}',
+    './pages/**/*.{js,ts,jsk,tsk}',
+    './components/**/*.{js,ts,jsk,tsk}',
+  ],
   theme: {
     extend: {},
   },
