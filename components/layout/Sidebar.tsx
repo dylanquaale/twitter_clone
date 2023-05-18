724bf0cdbcc8836eee04be8d4a1c8cc22962b5ca
dylanquaale <dylanquaale33@gmail.com>
2023-05-17 20:12:00 -0500

    element type issue

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,9 +1,11 @@
 import { BsHouseFill, BsBellFill } from 'react-icons/bs';
 import { FaUser } from 'react-icons/fa';
+// import { BiLogOut } from 'react-icons/bi'
 import React from 'react'; // Add this line if not already imported
 
 import SidebarItem from './SidebarItem';
 import SidebarLogo from './SidebarLogo';
+
 
 const Sidebar = () => {
   const items = [
@@ -23,7 +25,8 @@ const Sidebar = () => {
       icon: <FaUser /> // Wrap the icon component in JSX tags
     }
   ];
-
+      // col-span is the grid h-full height 100% pr-4 adds padding the right side, 
+      //md:pr-6 applies paddding when screen width reaches medium break point
   return (
     <div className='col-span-1 h-full pr-4 md:pr-6'>
       <div className='flex flex-col items-end'> {/* Corrected the class name */}
@@ -33,14 +36,64 @@ const Sidebar = () => {
             <SidebarItem
               key={item.href}
               href={item.href}
-              label={item.label}
               icon={item.icon}
+              label={item.label}
+              
             />
           ))}
-        </div>
+          {/* <SidebarItem onClick={() => {}} icon={BiLogOut} label="Logout"/> */}
+        </div> 
       </div>
     </div>
   );
 };
 
 export default Sidebar;
+
+// import { BsHouseFill, BsBellFill } from 'react-icons/bs';
+// import { FaUser } from 'react-icons/fa';
+// import { BiLogOut } from 'react-icons/bi';
+// import React from 'react';
+
+// import SidebarItem from './SidebarItem';
+// import SidebarLogo from './SidebarLogo';
+
+// const Sidebar = () => {
+//   const items = [
+//     {
+//       label: 'Home',
+//       href: '/',
+//       icon: <BsHouseFill /> // Wrap the icon component in JSX tags
+//     },
+//     {
+//       label: 'Notifications',
+//       href: '/notifications',
+//       icon: <BsBellFill /> // Wrap the icon component in JSX tags
+//     },
+//     {
+//       label: 'Profile',
+//       href: '/users/123',
+//       icon: <FaUser /> // Wrap the icon component in JSX tags
+//     }
+//   ];
+
+//   return (
+//     <div className="col-span-1 h-full pr-4 md:pr-6">
+//       <div className="flex flex-col items-end space-y-2 lg:w-[230px]"> {/* Corrected the class name */}
+//         <SidebarLogo />
+//         {items.map((item) => (
+//           <SidebarItem
+//             key={item.href}
+//             href={item.href}
+//             icon={item.icon}
+//             label={item.label}
+//           />
+//         ))}
+//         <SidebarItem onClick={() => {}} icon={BiLogOut} label="Logout" />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Sidebar;
+
--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -1,7 +1,41 @@
-const SidebarItem = () => {
-    return (
-        <div></div>
-    );
+import { IconType } from "react-icons";
+
+interface SidebarItemProps {
+    label: string;
+    href?: string;
+    icon: IconType;
+    onClick?: () => void;
 }
 
-export default SidebarItem
+const SidebarItem: React.FC<SidebarItemProps> = ({
+    label,
+    icon: Icon,
+    href,
+    onClick,
+}) => {
+    return (
+        <div className="flex flex-row items-center">
+           <div
+             className="
+               relative
+               rounded-full
+               h-14
+               w-14
+               flex
+               items-center
+               justify-content
+               p-4
+               hover:bg-slate-300
+               hover:bg-opacity-10
+               cursor-pointer
+               lg:hidden
+            "
+        >
+            <Icon size={28} color="white" />
+        </div>
+
+        </div>
+    );
+};
+
+export default SidebarItem;
