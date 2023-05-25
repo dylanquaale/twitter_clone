2d239c612b0c8a67e8acd89ec3bc53e17d655547
dylanquaale <dylanquaale33@gmail.com>
2023-05-25 17:25:49 -0500

    fixing folder problem

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import { isPropertyAccessChain } from 'typescript';
+// import { isPropertyAccessChain } from 'typescript';
 
 declare global {
     var prisma: PrismaClient | undefined
